79cf39ac84de378a85e7f3fc17914f2642b3d4e0
ankur-upflairs <ankur.upflairs@gmail.com>
2024-11-15 14:27:34 +0530

    fkd

---
+++ react/react basics new/src/components/Content.jsx
@@ -0,0 +1,20 @@
+let x=<div>
+<h1>good morning</h1>
+<p>hello my name is shubham</p>
+<input type="text" ></input>
+<br/>
+</div>
+
+function Content() {
+return <div>
+  <h1>this is content</h1>
+  </div>
+}
+function Footer() {
+return <div>
+  <h1>this is footer</h1>
+  </div>
+}
+
+export default Content
+export {Footer,x}
---
+++ react/react basics new/src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+function Header(){
+    return <div>
+      <h1 className='heading'>good morning</h1>
+      <p>hello my name is shubham</p>
+      <img src="/hamburger.jpg" alt="" width={200} />
+      <br/>
+    </div>
+  }
+
+export default Header
+
+// export default Header
+//named export
